c380a6d598dd837a6fbff258730a1ed0ebb84f1d
Heiko Vogeler <heiko.vogeler@hvo.de>
2018-07-24 09:50:59 +0200

    create button

--- src/MainMenu.tsx
+++ src/MainMenu.tsx
@@ -19,6 +19,9 @@ export class MainMenu extends React.Component<any, any> {
                                 <a className="nav-link" href="/tracks">Tracks <span className="sr-only">(current)</span></a>
                             </li>
                             <li className="nav-item">
+                                <a className="nav-link" href="/create">Create</a>
+                            </li>
+                            <li className="nav-item">
                                 <a className="nav-link" href="/docs">Documentation</a>
                             </li>
                         </ul>
--- src/PageTitle.tsx
+++ src/PageTitle.tsx
@@ -11,10 +11,15 @@ export class PageTitle extends React.Component<IPageTitleProps, any> {
 
     public render() {
         return (
-                <div className="container-fluid p-3 ">
-                    <h1 className="text-secondary display-4">{this.props.title}</h1>
-                    <hr/>
+            <div className="container-fluid row p-3" style={{verticalAlign : "middle"}}>
+                <div className="col-6 text-right">
+                    <span className="text-secondary display-4">{this.props.title}</span>
                 </div>
+                <div className="col-1">
+                    <button type="button" className="btn btn-light">Create</button>
+                </div>
+                < hr/>
+            </div>
         )
     }
 }
