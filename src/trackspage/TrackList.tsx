2d5b00b2ff52391b7dcf8b39a958953d66e26c0e
Heiko Vogeler <heiko.vogeler@hvo.de>
2018-09-25 15:16:47 +0200

    timestamp of track correct

--- src/trackspage/TrackList.tsx
+++ src/trackspage/TrackList.tsx
@@ -1,4 +1,5 @@
 import {action} from "mobx";
+import * as moment from "moment";
 import * as React from 'react';
 import {TrackTo} from "../transport/TrackTo";
 
@@ -20,7 +21,6 @@ export class TrackList extends React.Component<ITrackListProps, any> {
                 <table id="TrackList" className="table table-hover">
                     <thead className="thead-light">
                     <tr>
-                        <th scope="col">TrackId</th>
                         <th scope="col" className="text-left">Name</th>
                         <th scope="col">Country</th>
                         <th scope="col">Region</th>
@@ -34,11 +34,10 @@ export class TrackList extends React.Component<ITrackListProps, any> {
                             className={this.props.currentTrackListId === it.id ? "bg-info text-white" : "bg-white text-dark"}
                             onClick={() => this.onClick(it.id)}
                             onMouseEnter={() => this.onMouseEnter(it.id)}>
-                            <th scope="row">{it.id}</th>
                             <td className="text-left">{it.trackname}</td>
                             <td>{it.country}</td>
                             <td>{it.region}</td>
-                            <td>{it.created}</td>
+                            <td>{moment(it.created).format("Y-MM-DD HH:mm:ss")}</td>
                         </tr>
                     )}
                     </tbody>
